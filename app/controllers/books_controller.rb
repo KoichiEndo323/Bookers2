3ea38c4857cc62c65d9bc022f8c5e4845ee1b0a4
KouichiEndo <129310733+KoichiEndo323@users.noreply.github.com>
2023-04-18 15:34:58 +0000

    [Add]課題３

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,8 +6,12 @@ class BooksController < ApplicationController
    def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
-    @book.save
+    if @book.save
+    flash[:notice] = "投稿が成功しました"
     redirect_to books_path
+    else
+    render :new
+    end
    end
 
   def index
@@ -17,6 +21,16 @@ class BooksController < ApplicationController
 
   def show
     @book = Book.find(params[:id])
+  end
+
+  def edit
+   @book = Book.find(params[:id])
+  end
+
+  def update
+   @book = Book.find(params[:id])
+   @book.update(book_params)
+  redirect_to book_path(@book.id)
   end
 
   def destroy
